2df9040b43daa2bf26257eac72779d8a55ce6d2b
brianoliveira13 <sextapssss@gmail.com>
2025-08-08 23:34:33 -0400

    add server info fields

--- samp_server/handlers.py
+++ samp_server/handlers.py
@@ -7,6 +7,12 @@ def handle_server_info(bs: ByteStream, query: QueryPacket):
     response._stream.write(query.server_ip)
     response._stream.write(query.server_port)
     response._stream.write(bytes([ord('i')]))
+    response.write_num(0, 'B')
+    response.write_num(422, 'H') 
+    response.write_num(500, 'H')
+    response.write_str('Mega Bomba Roleplay', 'I')
+    response.write_str('RPG', 'I')
+    response.write_str('Spanish', 'I')
     return response.get()
 
 def handle_server_rules(bs: ByteStream, query: QueryPacket):
@@ -21,7 +27,8 @@ def handle_server_rules(bs: ByteStream, query: QueryPacket):
         "mapname": "San Andreas",
         "version": "0.3.7",
         "weather": "10",
-        "worldtime": "12:00"
+        "worldtime": "12:00",
+        "weburl": "https://github.com/brianoliveira13/fake-samp-server"
     }
     response.write_num(len(rules), 'H')
 
